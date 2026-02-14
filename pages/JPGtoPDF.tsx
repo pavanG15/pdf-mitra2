cd3e79adbb41e9b8608fc9876ccae6e3c9215a50
pavanG15 <pavankhate12@gmail.com>
2026-02-14 13:04:15 +0530

    Update JPGtoPDF.tsx

--- pages/JPGtoPDF.tsx
+++ pages/JPGtoPDF.tsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
 import Dropzone from '../components/Dropzone';
+import PDFPreview from '../components/MyPDFPreview';
 import { ProcessingState } from '../types';
 
 declare const jspdf: any;
